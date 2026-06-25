6704b62139ded76617b59df6bdd9e7de455f88f1
Promzy55 <osagieomozuwa72@gmail.com>
2026-06-25 15:18:39 +0100

    Add hamburger menu for mobile navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import arrow from '../../assets/icons8-arrow-32.png'
 import { CoinContext } from '../../context/CoinContext'
 import { WatchlistContext } from '../../context/watchlistContext'
 import { Link, useLocation } from 'react-router-dom'
 import StarButton from '../../components/StarButton/starButton'
+
 const Navbar = () => {
   const { setCurrency } = useContext(CoinContext)
   const { watchlist } = useContext(WatchlistContext)
   const location = useLocation()
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   const currencyHandler = (event) => {
     switch (event.target.value) {
@@ -28,6 +30,10 @@ const Navbar = () => {
 
   const isActive = (path) => location.pathname === path
 
+  const handleNavClick = () => {
+    setMobileMenuOpen(false)
+  }
+
   return (
     <div className='navbar'>
       <div className='brand'>
@@ -41,9 +47,33 @@ const Navbar = () => {
         </div>
       </div>
 
-      <ul className='nav-links'>
+      {/* Hamburger Menu Button */}
+      <button
+        className={`hamburger ${mobileMenuOpen ? 'active' : ''}`}
+        onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+        aria-label='Toggle menu'
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Mobile Menu Overlay */}
+      {mobileMenuOpen && (
+        <div
+          className='mobile-menu-overlay'
+          onClick={() => setMobileMenuOpen(false)}
+        />
+      )}
+
+      {/* Nav Links */}
+      <ul className={`nav-links ${mobileMenuOpen ? 'mobile-open' : ''}`}>
         <li>
-          <Link to='/' className={isActive('/') ? 'nav-active' : ''}>
+          <Link
+            to='/'
+            className={isActive('/') ? 'nav-active' : ''}
+            onClick={handleNavClick}
+          >
             Markets
           </Link>
         </li>
@@ -51,6 +81,7 @@ const Navbar = () => {
           <Link
             to='/watchlist'
             className={isActive('/watchlist') ? 'nav-active' : ''}
+            onClick={handleNavClick}
           >
             Watchlist
             {watchlist.length > 0 && (
@@ -62,6 +93,7 @@ const Navbar = () => {
           <Link
             to='/portfolio'
             className={isActive('/portfolio') ? 'nav-active' : ''}
+            onClick={handleNavClick}
           >
             Portfolio
           </Link>
